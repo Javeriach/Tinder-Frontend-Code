34ec9a6197023f651e7b12cde7a1f3e55793141e
Javeriach <javeriakanwal383@gmail.com>
2024-12-04 01:43:08 +0500

    Make the changes to the tailwindcss files

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,4 +61,8 @@ export default {
   daisyui: {
     themes: ['light', 'dark', 'cupcake', 'night'],
   },
+  content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}'],
+  // ...
+  safelist: ['bg-red-500', 'bg-green-500', 'bg-blue-500'],
+  // ...
 };
