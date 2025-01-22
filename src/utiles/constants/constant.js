e886546be76855db98caf666387337aed23dec5f
Javeriach <javeriakanwal383@gmail.com>
2025-01-22 17:25:57 +0500

    Feat:Update the profile

--- src/utiles/constants/constant.js
+++ src/utiles/constants/constant.js
@@ -1,2 +1,3 @@
-export let BASE_USL =
-  location.hostname === 'localhost' ? 'http://localhost:7777' : '/api';
+// export let BASE_USL =
+//   location.hostname === 'localhost' ? 'http://localhost:7777' : '/api';
+export let BASE_USL = '/api';
