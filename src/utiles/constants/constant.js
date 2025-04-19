c07d187123e00f340aaed0e34b218460aa9c896f
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-04-19 17:06:49 +0500

    Update constant.js

--- src/utiles/constants/constant.js
+++ src/utiles/constants/constant.js
@@ -1,4 +1,4 @@
 export let BASE_USL =
   location.hostname === 'localhost'
     ? 'http://localhost:7777'
-    : 'https://tinder-server-ten.vercel.app';
+    : 'https://tinder-server-2uuv.vercel.app';
