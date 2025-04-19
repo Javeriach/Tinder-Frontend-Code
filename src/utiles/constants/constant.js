92d7d00992037d877716d4461b7d45ddc764a6f8
Javeria Kanwal <109088965+Javeriach@users.noreply.github.com>
2025-04-19 15:49:35 +0500

    Update constant.js

--- src/utiles/constants/constant.js
+++ src/utiles/constants/constant.js
@@ -1,4 +1,4 @@
 export let BASE_USL =
   location.hostname === 'localhost'
     ? 'http://localhost:7777'
-    : 'https://corsproxy.io/?url=https://tinder-server-2uuv.vercel.app';
+    : 'https://tinder-server-2uuv.vercel.app';
